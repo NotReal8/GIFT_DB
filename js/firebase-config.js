d8d9d1a265d0441de518ef8392536430bd531c02
NotReal8 <129881019+NotReal8@users.noreply.github.com>
2026-05-13 12:46:36 +0530

    Replace Firebase config with actual project values
    Updated Firebase configuration with actual project credentials.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,14 @@
 // js/firebase-config.js
 // TODO: Replace with your actual Firebase project config
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  
+  apiKey: "AIzaSyBV84ua2Dye4h3ZiFRbGrIjih6Rlg_xZD4",
+  authDomain: "attendo-4192d.firebaseapp.com",
+  projectId: "attendo-4192d",
+  storageBucket: "attendo-4192d.firebasestorage.app",
+  messagingSenderId: "1071634729761",
+  appId: "1:1071634729761:web:be0b8d2d95d6bbc981cc60",
+  measurementId: "G-GRWYPSKJP4"
 };
 
 firebase.initializeApp(firebaseConfig);
