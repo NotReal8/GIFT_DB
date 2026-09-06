43968545ec255256d53359a68f122afc67efc892
SenTree <sentree.app@gmail.com>
2026-09-06 12:28:14 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,13 +2,13 @@
 // TODO: Replace with your actual Firebase project config
 const firebaseConfig = {
   
-  apiKey: "AIzaSyBV84ua2Dye4h3ZiFRbGrIjih6Rlg_xZD4",
-  authDomain: "attendo-4192d.firebaseapp.com",
-  projectId: "attendo-4192d",
-  storageBucket: "attendo-4192d.firebasestorage.app",
-  messagingSenderId: "1071634729761",
-  appId: "1:1071634729761:web:be0b8d2d95d6bbc981cc60",
-  measurementId: "G-GRWYPSKJP4"
+    apiKey: "AIzaSyC5u5bDABYcYrzIhDpikg4ltYECxkwFsMk",
+    authDomain: "sentree-bdee7.firebaseapp.com",
+    projectId: "sentree-bdee7",
+    storageBucket: "sentree-bdee7.firebasestorage.app",
+    messagingSenderId: "506288326530",
+    appId: "1:506288326530:web:19a71eeca41f33382c319b",
+    measurementId: "G-9Q3H4P2FJ1"
 };
 
 firebase.initializeApp(firebaseConfig);
